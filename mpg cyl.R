49bfcad7704d1fb002a3de46ddc77b9452db3bf9
Vasanth11022003 <113410656+Vasanth11022003@users.noreply.github.com>
2023-02-05 21:12:42 +0530

    Create mpg cyl.R

---
+++ mpg cyl.R
@@ -0,0 +1,11 @@
+input <- mtcars[,c('mpg','cyl')]
+print(head(input))
+
+OUTPUT:
+ mpg cyl
+Mazda RX4         21.0   6
+Mazda RX4 Wag     21.0   6
+Datsun 710        22.8   4
+Hornet 4 Drive    21.4   6
+Hornet Sportabout 18.7   8
+Valiant           18.1   6

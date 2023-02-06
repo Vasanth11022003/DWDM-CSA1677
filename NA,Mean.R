f3169c7aeae4229cb2e8d43213986fd0bb6e83da
Vasanth11022003 <113410656+Vasanth11022003@users.noreply.github.com>
2023-02-06 09:57:38 +0530

    Create NA,Mean.R

---
+++ NA,Mean.R
@@ -0,0 +1,14 @@
+ x <- c(12,7,3,4.2,18,2,54,-21,8,-5,NA)
+> result.mean <-  mean(x)
+> print(result.mean)
+
+
+answer:
+
+[1] NA
+> result.mean <-  mean(x,na.rm = TRUE)
+> print(result.mean)
+
+answer:
+
+[1] 8.22
